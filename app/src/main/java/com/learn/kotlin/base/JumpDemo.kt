1b5281442b94094c43d11de466125129aa366769
firsthubgit <1280209233@qq.com>
2019-08-06 14:57:57 +0800

    commit

--- app/src/main/java/com/learn/kotlin/base/JumpDemo.kt
+++ app/src/main/java/com/learn/kotlin/base/JumpDemo.kt
@@ -10,3 +10,43 @@ fun main(){
         }
     }
 }
+
+
+fun foo() {
+    listOf(1, 2, 3, 4, 5).forEach lit@{
+        if (it == 3) return@lit // 局部返回到该 lambda 表达式的调用者，即 forEach 循环
+        print(it)
+    }
+    print(" done with explicit label")
+}
+
+
+fun foo2() {
+    listOf(1, 2, 3, 4, 5).forEach {
+        if (it == 3) return@forEach // 局部返回到该 lambda 表达式的调用者，即 forEach 循环
+        print(it)
+    }
+    print(" done with implicit label")
+}
+
+//匿名函数包装
+fun foo3() {
+    listOf(1, 2, 3, 4, 5).forEach(fun(value: Int) {
+        if (value == 3) return  // 局部返回到匿名函数的调用者，即 forEach 循环
+        print(value)
+    })
+    print(" done with anonymous function")
+}
+
+
+//当需要break的时候，因为不是循环，不能用break
+//下面这种方式，模仿break
+fun foo4() {
+    run loop@{
+        listOf(1, 2, 3, 4, 5).forEach {
+            if (it == 3) return@loop // 从传入 run 的 lambda 表达式非局部返回
+            print(it)
+        }
+    }
+    print(" done with nested loop")
+}
