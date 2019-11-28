a4ed5ad4ba57b23aa4f825e3e60fd5cad56dc636
firsthubgit <1280209233@qq.com>
2019-11-28 17:58:43 +0800

    commit

--- app/src/main/java/com/learn/kotlin/high/Lambda.kt
+++ app/src/main/java/com/learn/kotlin/high/Lambda.kt
@@ -1,13 +1,29 @@
 package com.learn.kotlin.high
 
+/**
+ * Lambda 和 匿名函数 是未声明的函数
+ * */
 fun  main(){
-    //定义一个lambda
+
+    //定义一个lambda，lambda表达式总是在花括号中
     fun main(args: Array<String>) {
         //(Int, Int)表示参数类型
         // ->Int 表示返回值
         val sumLambda: (Int, Int) -> Int = {x,y -> x+y}
+        //自动推断类型的写法
+        val sum = {x :Int ,y: Int -> x +y}
         println(sumLambda(1,2))  // 输出 3
     }
+
+    //上面的Lambda表达式 等价于下面普通的命名函数
+    fun compare(a: Int, b: Int) : Int {
+        return a + b
+    }
+
+    //单体表达式  当函数返回的单个表达式时，
+    // 可以省略花括号并且在=符号之后指定代码体
+    fun compare1(a: Int, b: Int) : Int  = a + b
+
 
     //lambda  https://juejin.im/entry/58a382da61ff4b0058ab4542
     val numbers = mutableListOf("one", "two", "q","three")
@@ -40,6 +56,7 @@ fun  main(){
 
     //如果函数只有这么一个 Lambda 表达式参数，
     //前面那个不就是么，剩下一个小括号也没什么用，干脆也丢掉吧：
+    //如果该 lambda 表达式是调用时唯一的参数，那么圆括号可以完全省略
     numbers.forEach{
         if(it == "q") return
         println(it)
@@ -49,5 +66,21 @@ fun  main(){
     //如果这个 Lambda 表达式里面只有一个函数调用，
     //并且这个函数的参数也是这个Lambda表达式的参数
     numbers.forEach(::println)
+
+
+
+
+    //lambda中的返回一个值，默认最后一个表达式的值，最为返回值
+    var ints = arrayOf(1,2,3,4)
+
+    ints.filter {
+        val shouldFilter = it > 0
+        shouldFilter
+    }
+
+    ints.filter {
+        val shouldFilter = it > 0
+        return@filter shouldFilter
+    }
 
 }
